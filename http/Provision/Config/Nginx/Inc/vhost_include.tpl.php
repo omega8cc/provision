cd2a75d6b138f783318b43fb50abfbbd8c4da1fa
BOA Dev Team <boa@omega8.cc>
2018-11-23 22:47:06 +0100

    Nginx: Support for dynamic /sw.js requests.
    See #2982073 on drupal.org

--- http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
+++ http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
@@ -765,6 +765,13 @@ location ~* \.css$ {
 }
 
 ###
+### Support for dynamic /sw.js requests. See #2982073 on drupal.org
+###
+location = /sw.js {
+  try_files $uri @cache;
+}
+
+###
 ### Make js files compatible with boost caching.
 ###
 location ~* \.(?:js|htc)$ {
