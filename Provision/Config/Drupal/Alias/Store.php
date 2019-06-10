7e4d277f0f9bbb7d9381ff1b4c16687a1adc96d2
BOA <boa@omega8.cc>
2019-06-10 05:22:30 +0200

    Fix the logic

--- Provision/Config/Drupal/Alias/Store.php
+++ Provision/Config/Drupal/Alias/Store.php
@@ -10,11 +10,8 @@ class Provision_Config_Drupal_Alias_Store extends Provision_Config_Data_Store {
   public $key = 'sites';
   protected $mode = 0644;
 
-  // Add platform root auto-discovery to avoid confusing
-  // Composer based D8 codebase root with Drupal real root.
-  $this->root = provision_auto_fix_platform_root($this->root);
-
   function filename() {
+    $this->root = provision_auto_fix_platform_root($this->root);
     return $this->root . '/sites/sites.php';
   }
 
--- Provision/Config/Drushrc/Platform.php
+++ Provision/Config/Drushrc/Platform.php
@@ -13,11 +13,8 @@ class Provision_Config_Drushrc_Platform extends Provision_Config_Drushrc {
   // platforms contain no confidential information
   protected $mode = 0444;
 
-  // Add platform root auto-discovery to avoid confusing
-  // Composer based D8 codebase root with Drupal real root.
-  $this->root = provision_auto_fix_platform_root($this->root);
-
   function filename() {
+    $this->root = provision_auto_fix_platform_root($this->root);
     return $this->root . '/sites/all/drush/drushrc.php';
   }
 }
