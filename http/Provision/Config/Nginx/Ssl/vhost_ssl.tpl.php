566c12c8ce6736bdf460f2b1676d0795d7f47f35
BOA <boa@omega8.cc>
2019-10-15 03:28:18 +0200

    Nginx: fastcgi_param always in ssl tpl

--- http/Provision/Config/Nginx/Ssl/vhost_ssl.tpl.php
+++ http/Provision/Config/Nginx/Ssl/vhost_ssl.tpl.php
@@ -99,7 +99,6 @@ server {
 <?php endif; ?>
 
 server {
-<?php if (!$this->redirection && !$ssl_redirection): ?>
   include       fastcgi_params;
   # Block https://httpoxy.org/ attacks.
   fastcgi_param HTTP_PROXY "";
@@ -107,7 +106,6 @@ server {
   set $main_site_name "<?php print $this->uri; ?>";
   fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
   fastcgi_param HTTPS on;
-<?php endif; ?>
 <?php
   // If any of those parameters is empty for any reason, like after an attempt
   // to import complete platform with sites without importing their databases,
@@ -122,13 +120,11 @@ server {
     $db_host = 'localhost';
   }
 ?>
-<?php if (!$this->redirection && !$ssl_redirection): ?>
   fastcgi_param db_type   <?php print urlencode($db_type); ?>;
   fastcgi_param db_name   <?php print urlencode($db_name); ?>;
   fastcgi_param db_user   <?php print urlencode($db_user); ?>;
   fastcgi_param db_passwd <?php print urlencode($db_passwd); ?>;
   fastcgi_param db_host   <?php print urlencode($db_host); ?>;
-<?php endif; ?>
 <?php
   // Until the real source of this problem is fixed elsewhere, we have to
   // use this simple fallback to guarantee that empty db_port does not
@@ -143,9 +139,7 @@ server {
     }
   }
 ?>
-<?php if (!$this->redirection && !$ssl_redirection): ?>
   fastcgi_param db_port   <?php print urlencode($db_port); ?>;
-<?php endif; ?>
   listen        <?php print "{$ssl_listen_ipv4}:{$http_ssl_port} {$ssl_args}"; ?>;
   #listen        <?php print "{$ssl_listen_ipv6}:{$http_ssl_port} {$ssl_args}"; ?>;
   server_name   <?php
