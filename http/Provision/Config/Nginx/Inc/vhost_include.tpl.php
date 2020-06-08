de3906e5f6e29657b9434170458b11c555217a13
Adam Andrzej Jaworski <mem@tkm.cc>
2020-06-08 19:58:29 +0200

    Issue #3149961: Nginx - Use 10 seconds micro-cache by default

--- http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
+++ http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
@@ -1251,7 +1251,7 @@ location ~ ^/(?<esi>esi/.*)"$ {
   fastcgi_cache_methods GET HEAD;
   fastcgi_cache_min_uses 1;
   fastcgi_cache_key "$scheme$is_bot$device$host$request_method$key_uri$cache_uid$http_x_forwarded_proto$sent_http_x_local_proto$cookie_respimg";
-  fastcgi_cache_valid 200 3s;
+  fastcgi_cache_valid 200 10s;
   fastcgi_cache_valid 301 302 403 404 1s;
   fastcgi_cache_valid any 1s;
   fastcgi_cache_lock on;
