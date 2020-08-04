4afc72dfbec0b3a8483b2a861618304c0a1d6662
Jon Pugh <jon@thinkdrop.net>
2020-08-03 23:06:20 -0400

    Revert "Issue #3149998: Auto-detect Drupal 8 and 9 codebase structure"
    This reverts commit 3de1a9e8bf106aa7b9523ce5de35cc5fec705d45.

--- Provision/Config/Drupal/provision_drupal_settings_9.tpl.php
+++ Provision/Config/Drupal/provision_drupal_settings_9.tpl.php
@@ -202,7 +202,6 @@ if (isset($_SERVER['db_name'])) {
   }
 
   # Additional platform wide configuration settings.
-  <?php $this->platform->root = provision_auto_fix_platform_root($this->platform->root); ?>
   if (is_readable('<?php print $this->platform->root  ?>/sites/all/platform.settings.php')) {
     include('<?php print $this->platform->root ?>/sites/all/platform.settings.php');
   }
