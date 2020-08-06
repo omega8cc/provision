bdaccaf0bf5d5662c807b7f6b1fec2457ab81304
Jon Pugh <jon@thinkdrop.net>
2020-08-06 15:25:42 -0400

    Don't forget this is PRINTING php code :|

--- Provision/Config/Drushrc/provision_drushrc_aegir.tpl.php
+++ Provision/Config/Drushrc/provision_drushrc_aegir.tpl.php
@@ -15,7 +15,7 @@ print "\$options['hosting_features'] = ". var_export($hosting_features, TRUE) . 
 
 print "# A list of modules to be excluded because the hosting feature is not enabled.\n";
 foreach ($drush_exclude as $key => $value) {
-  print "\$options['exclude'][$key] = $value;\n";
+  print "\$options['exclude']['$key'] = '$value';\n";
 }
 
 print "# Drush 8 looks at ignored-modules instead of exclude.\n";
@@ -24,7 +24,7 @@ print "\$options['ignored-modules'] = \$options['exclude'];\n\n";
 print "# A list of paths that drush should include even when working outside\n";
 print "# the context of the hostmaster site.\n";
 foreach ($drush_include as $key => $value) {
-  print "\$options['include'][$key] = $value;\n";
+  print "\$options['include']['$key'] = '$value';\n";
 }
 
 print "# Local non-aegir-generated additions.\n";
