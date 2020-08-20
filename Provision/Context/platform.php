48b0568bed0cd962268a0f9d29147664ebb1be52
Jon Pugh <jon@thinkdrop.net>
2020-08-20 08:49:15 -0400

    Indentation.

--- Provision/Context/platform.php
+++ Provision/Context/platform.php
@@ -18,9 +18,9 @@ class Provision_Context_platform extends Provision_Context {
       'web_server' => 'platform: web server hosting the platform; default @server_master',
       'makefile' => 'platform: drush makefile to use for building the platform if it doesn\'t already exist',
       'make_working_copy' => 'platform: Specifiy TRUE to build the platform with the Drush make --working-copy option.',
-        'git_root' => 'platform: The absolute path to clone the git repository to. May be left empty if same as document root. If the document root is inside a subfolder of the git repository, be sure that the "root" property points to the web document root.',
-        'git_remote' => 'platform: The URL of the git repository to use when creating this platform.',
-        'git_reference' => 'platform: The git reference to check out. Can be a branch, tag or SHA. Defaults to the git repository default branch.',
+      'git_root' => 'platform: The absolute path to clone the git repository to. May be left empty if same as document root. If the document root is inside a subfolder of the git repository, be sure that the "root" property points to the web document root.',
+      'git_remote' => 'platform: The URL of the git repository to use when creating this platform.',
+      'git_reference' => 'platform: The git reference to check out. Can be a branch, tag or SHA. Defaults to the git repository default branch.',
     );
   }
 
