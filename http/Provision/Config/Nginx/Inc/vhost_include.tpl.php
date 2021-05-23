db60ed894342601dbb5e261c0a754a54771c54b3
BOA Dev Team <boa@omega8.cc>
2021-05-23 21:59:15 +0200

    Issue #3213279 by bwaindwain: Fix regressions

--- http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
+++ http/Provision/Config/Nginx/Inc/vhost_include.tpl.php
@@ -356,7 +356,7 @@ location ^~ /js/ {
     if ( $is_bot ) {
       return 403;
     }
-    rewrite ^/(.*)$ /js.php?q=$1? last;
+    rewrite ^/(.*)$ /js.php?q=$1 last;
   }
 }
 
@@ -1367,7 +1367,7 @@ location @drupal {
 ###
 location @legacy {
   set $location_detected "Legacy";
-  rewrite ^/(.*)$ /index.php?q=$1? last;
+  rewrite ^/(.*)$ /index.php?q=$1 last;
 }
 
 ###
