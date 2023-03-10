aa4326f101c38c717d2a60d570c87afd0e940e95
Jon Pugh <jon@thinkdrop.net>
2023-03-10 15:00:46 -0500

    Clean up git display

--- Provision/Context/platform.php
+++ Provision/Context/platform.php
@@ -213,21 +213,9 @@ class Provision_Context_platform extends Provision_Context {
   /**
    * Print out current git status, git remotes, and logs.
    */
-  public function displayGitStatus($remote_show = false) {
-
-    // Output Git Information
+  public function displayGitStatus() {
     $provision_log_type = drush_get_option('runner') == 'hosting_task'? 'p_info': 'ok';
-    provision_process(['git', 'show',  '--compact-summary'], $this->git_root, dt('Current HEAD'), [], TRUE, NULL, TRUE, $provision_log_type);
-    provision_process(['git', 'status', '--ahead-behind'], $this->git_root, dt('Git status'), [], TRUE, NULL, TRUE, $provision_log_type);
-
-    if ($remote_show) {
-      provision_process(['git', 'remote',  '--verbose'], $this->git_root, dt('Git Remotes'), [], TRUE, NULL, TRUE, $provision_log_type);
-    }
-
-    // If there is a current remote, show it (includes access check.)
-    if ($remote_show && $remote_name = $this->getCurrentRemoteName()) {
-      provision_process(['git', 'remote', 'show', $remote_name], $this->git_root, dt('Current Remote Status'), [], TRUE, NULL, TRUE, $provision_log_type);
-    }
+    provision_process("git -c color.ui=always log --oneline --max-count=1 && git -c color.ui=always status && git -c color.ui=always diff --minimal", $this->git_root, dt('Git Status'), [], TRUE, dt('Unable to run git status.'), FALSE, $provision_log_type);
   }
 
   /**
