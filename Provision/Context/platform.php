938b1e84260ae678aae07746c765820dba4c11a4
Jon Pugh <jon@thinkdrop.net>
2023-03-20 13:52:26 -0400

    Reduce stuff being saved from git. Show --compact-summary and no diff.

--- Provision/Context/platform.php
+++ Provision/Context/platform.php
@@ -98,8 +98,7 @@ class Provision_Context_platform extends Provision_Context {
     chdir($this->git_root);
     return implode(PHP_EOL . PHP_EOL, [
       trim(shell_exec("git -c color.ui=always status 2> /dev/null")),
-      trim(shell_exec("git -c color.ui=always show 2> /dev/null")),
-      trim(shell_exec("git -c color.ui=always diff --minimal 2> /dev/null")),
+      trim(shell_exec("git -c color.ui=always show --compact-summary 2> /dev/null")),
     ]);
   }
 
