283b2f0bf1179ecb2095a313b580c953bf055f19
memtkmcc <mem@tkm.cc>
2023-11-08 14:57:35 +0100

    Revert "Fix syntax"
    This reverts commit 85b2d77902120f87fa91d832e58fdc7350d380f3.

--- Provision/Config/Drushrc/Alias.php
+++ Provision/Config/Drushrc/Alias.php
@@ -26,10 +26,9 @@ class Provision_Config_Drushrc_Alias extends Provision_Config_Drushrc {
       $data['drush_aliases'] = array_unique($data['drush_aliases']);
     }
 
-    // Ignore site-local drush and use the Octopus local drush
+    // ignore site-local drush and use the Octopus local drush
     if (!empty(d()->drush_script)) {
       $data['path-aliases']['%drush-script'] = d()->drush_script;
-    }
     else {
       $octopus_drush_script = d('@server_master')->aegir_root . "/tools/drush/drush.php";
       $data['path-aliases']['%drush-script'] = $octopus_drush_script;
