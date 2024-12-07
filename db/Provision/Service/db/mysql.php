ab20d2ae0481298901de3d184fe50d95a1a9e11c
BOA Dev Team <boa@omega8.cc>
2024-12-07 13:13:38 +0100

    Restore the comment

--- db/Provision/Service/db/mysql.php
+++ db/Provision/Service/db/mysql.php
@@ -161,6 +161,7 @@ class Provision_Service_db_mysql extends Provision_Service_db_pdo {
       return $user_altered;
     }
 
+    // MySQL did this to us. https://github.com/drush-ops/drush/issues/5368#issuecomment-1405209770
     $statement = "GRANT ALL PRIVILEGES ON `%s`.* TO `%s`@`%s`";
     return $this->query($statement, $name, $username, $host);
   }
