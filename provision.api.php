8c8bf1f001a92a0013ccd975f6eb9a2adfd12396
BOA Dev Team <boa@omega8.cc>
2024-12-10 00:59:22 +0100

    Fix provision_backup_suffix to use .tar

--- provision.api.php
+++ provision.api.php
@@ -19,7 +19,7 @@
  *
  * provision_backup_suffix
  *   Method to set the compression used for backups... e.g. '.tar.gz', '.tar.bz2', '.tar.zst',
- *    '.tar.xz', '.tar.lz4' or '.tar.', defaults to '.tar.'
+ *    '.tar.xz', '.tar.lz4' or '.tar', defaults to '.tar'
  *
  * provision_apache_conf_suffix
  *   Set to TRUE to generate apache vhost files with a .conf suffix, default FALSE.
