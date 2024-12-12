c214ba5047d4d17ba06cabff359278df398a6e7b
BOA Dev Team <boa@omega8.cc>
2024-12-12 12:46:33 +0100

    Use supported localhost in can_grant_privileges()

--- db/Provision/Service/db/mysql.php
+++ db/Provision/Service/db/mysql.php
@@ -61,7 +61,7 @@ class Provision_Service_db_mysql extends Provision_Service_db_pdo {
     $this->create_database($dbname);
     $user     = $dbname . '_user';
     $password = $dbname . '_password';
-    $host     = $dbname . '_host';
+    $host     = 'localhost';
     $status = $this->grant($dbname, $user, $password, $host);
     $this->revoke($dbname, $user, $host);
     $this->drop_database($dbname);
